f1f10403f39e219de69f79dae70e63f7c22a5077
kvin1087 <kaustubh87@gmail.com>
2017-08-04 16:10:02 -0500

    Changed the create function

--- MEAN application/app/models/User.js
+++ MEAN application/app/models/User.js
@@ -1,12 +1,28 @@
 var mongoose = require('mongoose');
 
 
-var UserSchema =  mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    username: String,
-    password: String
+var UserSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+
 });
 
 mongoose.model('User', UserSchema);
