65e969d0e38125b2b8929ec3a3980fe8210ebf77
kvin1087 <kaustubh87@gmail.com>
2017-08-04 16:10:11 -0500

    Changed the function

--- MEAN application/app/controllers/userController.js
+++ MEAN application/app/controllers/userController.js
@@ -2,21 +2,19 @@ var mongoose = require('mongoose');
 
 var User = mongoose.model('User');
 
-module.exports.addUser = function(req,res){
+module.exports.addUser = function(req, res) {
 
-        User.create({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            username: req.body.username,
-            password: req.body.password
-        }, function(err, result){
-            if(err){
-                throw err;
-            }
-            else{
-            console.log('User added' +result);
-            }
-        });
+    User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    }, function(err, result) {
+        if (err) {
+            throw err;
+        } else {
+            console.log('User added' + result);
+        }
+    });
 };
-
