b123d5e05b9d16e9993c33c20b92368a6de5de23
kvin1087 <kaustubh87@gmail.com>
2017-08-06 18:09:02 -0500

    changed callback function

--- MEAN application/app/controllers/userController.js
+++ MEAN application/app/controllers/userController.js
@@ -4,6 +4,8 @@ var User = mongoose.model('User');
 
 module.exports.addUser = function(req, res) {
 
+
+
     User.create({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -11,10 +13,8 @@ module.exports.addUser = function(req, res) {
         username: req.body.username,
         password: req.body.password
     }, function(err, result) {
-        if (err) {
-            throw err;
-        } else {
-            console.log('User added' + result);
-        }
+
+        console.log('User added' + result);
+
     });
 };
