46d0b2a18d3607067d5e07af7fdcf7908cd4a8cb
kvin1087 <kaustubh87@gmail.com>
2017-08-11 11:23:45 -0500

    Added passport and flash messages

--- MEAN application/app/controllers/userController.js
+++ MEAN application/app/controllers/userController.js
@@ -1,7 +1,28 @@
 var mongoose = require('mongoose');
 
 var User = mongoose.model('User');
+var passport = require('passport');
 
+var getErrorMessage = function(err) {
+    var message = '';
+
+    if (err.code) {
+        switch (err.code) {
+            case 11000:
+            case 11001:
+                message = 'Username already exists';
+                break;
+            default:
+                message = 'Something went wrong';
+        }
+    } else {
+        for (var errName in err.errors) {
+            if (err.errors[errName].message) message = err.errors[errName].message;
+        }
+    }
+
+    return message;
+};
 
 module.exports.addUser = function(req, res) {
 
