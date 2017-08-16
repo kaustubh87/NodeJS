33ed4229e53a13ebadd605cc14dad8ae91cea6e1
kvin1087 <kaustubh87@gmail.com>
2017-08-16 11:28:32 -0500

    Added ngRoute as dependency

--- MEAN application/public/application.js
+++ MEAN application/public/application.js
@@ -1,6 +1,6 @@
 var mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
 
 angular.element(document).ready(function() {
     angular.bootstrap(document, [mainApplicationModuleName]);
