ebebdb8becc3a56a0ced1730a87d7189be5d8413
kvin1087 <kaustubh87@gmail.com>
2017-08-16 11:47:07 -0500

    added locationProvider with hashbang

--- MEAN application/public/application.js
+++ MEAN application/public/application.js
@@ -2,6 +2,10 @@ var mainApplicationModuleName = 'mean';
 
 var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
 
+mainApplicationModule.config(['$locationProvider', function($locationProvider) {
+    $locationProvider.hashPrefix('!');
+}]);
+
 angular.element(document).ready(function() {
     angular.bootstrap(document, [mainApplicationModuleName]);
 });
