6a817e2099cc2854c2e2c0a573f307aaf6c10ed4
kvin1087 <kaustubh87@gmail.com>
2017-08-20 19:57:35 -0500

    Added new routes for Articles

--- MEAN application/app/routes/articles.js
+++ MEAN application/app/routes/articles.js
@@ -7,3 +7,8 @@ var app = express.Router();
 app.route('/api/articles')
     .get(articles.list)
     .post(users.requiresLogin, articles.create);
+
+app.route('/api/articles/:articleId')
+    .get(articles.read)
+    .put(users.requiresLogin, articles.hasAuthorization, articles.update)
+    .delete(users.requiresLogin, articles.hasAuthorization, articles.delete);
